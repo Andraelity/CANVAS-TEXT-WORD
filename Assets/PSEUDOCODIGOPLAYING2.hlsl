70b32f88d51754e22134b7fce55de78c583a2366
Andraelity <Andraelity@gmail.com>
2022-04-03 15:01:51 -0700

    First commit

---
+++ Assets/PSEUDOCODIGOPLAYING2.hlsl
@@ -0,0 +1,101 @@
+
+// learning how to represent more elements, learning how to integrate more propositions in the now, believing in the current motion of the mind, learning how to visaulize more interactions
+// learning how to approach more representations, learning how to construct a motion in the now loaded with more values, learning how to define new ideas in the present time, learning how to
+// visualize more definitions of the psyche, learning how to manipule a conception in the psyche, learning how to activate new representations.
+// we need to make reality new visions, we need to promote the definition capable to set more actions.
+
+// sensing how to set more behaviors, learning how to integrate more definitions in the now, learning how to manipule more data, learning the present representation of the world.
+
+// WITH ONE HONDRED THOUSAND LINES OF CODE I CAN OBTAIN A CLEAR DEFINITION OF THE OBJECTIVE.
+
+// WHAT IS THE ELEMENT I AM CURRENTLY HANDLING, LEARNING HOW TO ACQUIRE MORE REPRESENTATIONS OF THE PSYCHE.
+
+// WAHT IS THE PRESENT DESCRIPTION OF THE VALUES I NEED TO OBTAIN, LEARNING HOW TO INTEGRATE MORE DEFINITIONS, LEARNING HOW TO RECREATE A COMPONENT.
+
+// THIS IS THE GAME MECHANICS WE ARE TRYING TO ACHIEVE, THESE ELEMENTS ARE THE QUALITIES WE ARE TRYING TO APPROACH, THE CHARACTERISTICS WE NEED TO RESOLVE
+// BY LEARNING HOW TO INTEGRATE MORE BEHAVIORS.
+
+// what we are learning is something related with mind reproduction, trying to learn how to produce more componetns.
+
+
+/// defininig a set of code capable to make definitions in the now, learning how to acquire more representations in the present time, learning how to believe in the current description of the psyche
+// approaching a clear description of the psyche, modeling a vision capable to define more realities.
+
+
+/// using the code in the developement of more psyche, learning how to manipule more ideas in the present field of motion, defining a clear description of the values.
+
+
+// what is the main objective in this set of details, by learning how to integrate more behaviors, by learning how to manipule more ideas, by learning how to integrate more elements in the now
+// creating a description in the present time capable to set more realities.
+// learning how to integrate more representations.
+
+// defining more ideas in the present time capable to make reality all kind of behaviors, enhancing a quality loaded with more elements.
+
+
+// sensing how to approach more definitions, learning how to integrate more values, defining a clear representation of the now, modeling a perception capable to set more descriptions in the time.
+
+// generating a clear representation of the elements capable to set in life more behaviors, a element in life capable to define more ideas, a elemetn in life capable to structure more characteristics.
+
+
+// creating a definition capable to define more definitions, a relationship with the present time capable to set more behaviors, learning how to integrate more elements.
+
+/// what is the main idea in the now capable to set more realities, by learning how to integrate more definitions, by learning how to modulate more elements, by defining a clear path in the now loaded with more 
+// descriptions sensing how to approahc more behaviors, defining a clear representation of the qualities, enhancing the definition of the now capable to set more qualities, learning how to make reality new data, new
+// ways to maximize the present time, enhancing a representation of the visions, providing a clear idea of the psyche, providing a new way to define more elements.
+
+/// estoy viendo la maquina en mi mente, definiendo como los procesos se pueden desarrollar.
+
+
+// LET ME USE A DEFINITION WITH CODE CAPABLE TO RECREATE A TON OF LIENS OF CODE, LIKE TRILLIONS, TRILLIONS OF LIENS OF CODE.
+/// A MOVEMENT CAPABLE TO SET MORE REALITIES, A DESCRIPTION CAPABLE TO MAXIMIZE WHAT WE COULD BECAME.
+
+// WE START TO OPEN THINGS IN ORDER TO STUDY COMPONENTS, BY LEARNING HOW TO INTEGRATE MROE REPRESENTATIONS, OF THE ELEMENTS, CREATING A DEFINITION CAPABLE TO SET MORE BEHAVIORS.
+
+
+// SENSING HOW TO MANIPULE MORE DESCRIPTIONS IN THE NOW, LEARNING HOW TO APPROACH MORE REPRESENTATIONS OF THE PSYCHE, LEARNING HOW TO ACQUIRE MORE PROJECTS.
+
+
+// HOW IT LOOKS ONE HUNDRED LINES OF CODE.
+
+
+// WHAT IS THE PERSONALITY I NEED TO SET IN ORDER TO GENERATE NEW BEHAVIORS, BY LEARNING HOW TO MANIPULE MORE REALITIES IN THE PRESENT TIME, BY LEARNIGN HOW TO DEFINE MORE REPRESENTATION BY INCREASING THE VISIONS OF THE NOW
+// LEARNING HOW TO MANIPULE MORE DATA.
+
+
+
+
+// WHAT IS THE BEHAVIOR NEEDED IN THE NOW CAPABLE TO PRODUCE MORE QUALITIES, A MOTION CAPABLE TO SET MORE ACTIONS, A DEFINITON LOADED WITH MORE REPRESENTATIONS.
+
+
+// LEARNING HOW TO GENERATE MORE QUALITIE.
+
+// DEFINING A REPRESENTATION IN THE PRESENT TIME CAPABLE TO DEFINE MORE ACTIVITIES, BY LEARNING HOW TO APPROACH MORE EXPRESSIONS IN TEH PSYCHE.
+
+
+// A DEFINITION WE CAN APPPROAHC BETTER AND BETTER, CLEARLER AND CLEARLER.
+
+// CUANDO METO PRESION, LEARNING WHAT IS THE DEFINITION OF THE DATA, SENSING HOW REPRESENTATIONS, LEARNING HOW TO INTEGRATE MORE DATA, SENSING HOW TO PERSONALIZE MORE QUALITIES.
+
+
+// WE NEED TO DEFINE A COMPONENT A WAY TO MAXIMIZE THE VISIONS.
+
+
+// LEARNING HOW TO INTERACT WITH THE MATTER, SENSING HOW TO DEFINE MORE REPRESENTATIONS OF THE NOW, LEARNING HOW TO INTEGRATE MORE CONCEPTIONS
+// GENERATING VALUES AND MODELS, LEARNING HOW TO MODIFY MORE CORRELATIONS..
+
+
+// WHAT KIND OF DETAIL ENABLE IN MY MIND THE KIND OF PRODUCTION CAPABLE TO DEFINE MORE REPRESENTATIONS IN THE NOW, LEARNING HOW TO VISUALIZE MORE PERSONALITIES.
+
+
+// ENHANCING A MODEL IN THE PRESENT TIME CAPABLE TO DEFINE MORE REALTIES, LEARNING HOW TO BELIEVE MODELS OF VIEW, CREATING A PARTICULAR VISIOSN IN THE WORLD.
+
+
+// THIS THING IS TO CHIMBA, PLAYING THOSE GAMES IN MY HEAD, LEARNING HOW TO PROVIDE MORE RPERESENTATIONS IN THE NOW, LEARNING HOW TO ENHANCE THE PRODUCTION OF DATA, LEARNING HOW TO SINTETIZE MORE VALUES.
+
+// LETS GO ATOPE.
+
+// DEFINIING A ELEMENT CAPABLE TO MAKE REALITY NEW VISIONS IN THE PRESENT TIME SENSING HOW TO APPROACH MORE REALIITIES, LEARNING HOW TO INTEGRATE MORE DEFINITIONS.
+
+// WHAT IS THE KIND OF DRAW WE NEED TO DEFINE IN ORDER TO REPRESENT MORE DETAILS, WHAT IS THE MAIN VISION IN THIS COMPONENT, LEARNING HOW TO MODEL NEW REPRESENTATIONS,
+
+// LEARNING HOW TO APPROXIMATE NEW DETAILS, LEARNING HOW TO MODEL A CLEAR VISION OF THE PRESENT TIME.
